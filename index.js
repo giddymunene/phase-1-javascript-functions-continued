dd5adb5f9fc6c0d96f716f6709e581226bf6fc8e
gideon <gideonmunene0@gmail.com>
2024-03-25 10:52:08 -0400

    continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(someValue="roller-skate"){
+    return`This Saturday, I want to ${someValue}!`;
+    }
+console.log(saturdayFun("code"))
+
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`}
+console.log(mondayWork("work from home"))
+
+function wrapAdjective(text = "*"){
+    return function(msg ="special"){
+        return`You are *${msg}*!`;
+      }
+}
+wrapAdjective("%")("a dedicated programmer");
+console.log(wrapAdjective("*")("a hard worker"))
